389607a26af37ed81f4d9c21aa65fb1ecc6ea9a1
cobelu <imamac96@gmail.com>
2020-12-02 14:03:56 -0600

    splits

--- scheduler.py
+++ scheduler.py
@@ -1,38 +1,54 @@
 from typing import Type
 import numpy as np
 import ray
-from scipy.sparse import coo_matrix, csr_matrix, load_npz
+from scipy.sparse import coo_matrix, csr_matrix, csc_matrix, load_npz
+
+from work import Work
 
 
 @ray.remote
 class Scheduler(object):
 
-    def __init__(self):
+    def __init__(self, work: Work):
         # Parameters
         self.k = 100
         self.alpha = 0.08
         self.beta = 0.05
         self.lamda = 1
         self.file = ""
-        # Sparse data
-        self.a_coo: coo_matrix = load(self.file)
-        self.a_csr: csr_matrix = self.a_coo.tocsr()
-        # Data
+        # Sparse data is loaded as a CSR, sliced, and converted to CSC
+        self.a_csc: csc_matrix = load(self.file)[work.low:work.high].tocsc()
+        # Data to be found
         self.w = np.rand.random(self.k) * 1.0/np.sqrt(self.k)
         self.h = np.rand.random(self.k) * 1.0/np.sqrt(self.k)
 
-    def sgd(self):
-        for i, j, v in zip(self.a_coo.row, self.a_coo.col, self.a_coo.data):
+    def sgd(self, work: Work):
+        # Keeping track of RMSE along the way
+        nnz_ctr = 0
+        sum = 0
+        offset = work.low
+        # Mark the low and high
+        low = work.low - offset
+        high = work.high - offset
+        nnz = self.a_csc[low:high, :].getnnz()
+        nz_ids = self.a_csc.nonzero()
+        for i, j in nz_ids:
             # Get the respective entries
+            aij = self.a_csc[i][j]
             wi = self.w[i]
             hj = self.h[j]
             # error = [(Wi • Hj) - Aij]
-            err = self.a_coo[i][j] - np.dot(wi, hj)
+            err = aij - np.dot(wi, hj)
             tmp = wi
             # Wi -= lrate * (err*Hj + lambda*Wi)
             wi -= self.alpha * (err*hj + self.lamda*wi)
             # Hj -= lrate * (err*tmp + lambda*Hj);
             hj -= self.alpha * (err*tmp + self.lamda*hj)
+            # Calculate Error
+            entry = np.Dot(wi, hj)  # yi'
+            term = np.power(entry - aij, 2)  # (yi' - yi)^2
+            sum += term  # Σ_i=1 ^ n(yi' - yi)^2
+        nnz_ctr += nnz
 
 
 def load(filename: str):
--- work.py
+++ work.py
@@ -3,5 +3,31 @@ from typing import Type
 
 class Work:
     def __init__(self, low: int, high: int):
+        assert low <= high, "Low should be greater than high"
         self.low = low
         self.high = high
+
+    def splits(self, n: int):
+        dim = self.dim()
+        avg = dim // n
+        extra = self.dim() % n
+        # Add everything that's average to the list
+        works = []
+        last = self.low
+        for i in range(n-extra):
+            nxt = last + avg
+            works.append(Work(last, nxt))
+            last = nxt
+        # A few will be "extra", so they are added second
+        for i in range(n-extra, n):
+            nxt = last + avg + 1
+            works.append(Work(last, nxt))
+            last = next
+        return works
+
+    def individuals(self):
+        n = self.dim()
+        return [Work(self.low + i, self.low + (i + 1)) for i in range(n)]
+
+    def dim(self):
+        return self.high - self.low
